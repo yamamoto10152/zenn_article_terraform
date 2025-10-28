8eb64ea67aa7b8fbb4f9464e939fecfe1bd3c499
yamamoto10152 <yamamoto10152@ashisuto.co.jp>
2025-10-28 13:44:28 +0900

    chage modules4

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,6 +18,9 @@ terraform {
 
 module "yamamoto_terraform_db" {
   source = "../../modules/horizon_catalog/yamamoto_terraform_db"
+  providers = {
+    snowflake = snowflake.administrator
+  }
 }
 
 module "users_roles" {
