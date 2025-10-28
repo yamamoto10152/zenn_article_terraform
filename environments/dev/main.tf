5f043b0287bcc8c38560f58eaa8e41d611c45e39
yamamoto10152 <yamamoto10152@ashisuto.co.jp>
2025-10-28 13:48:29 +0900

    change modules5

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -18,12 +18,12 @@ terraform {
 
 module "yamamoto_terraform_db" {
   source = "../../modules/horizon_catalog/yamamoto_terraform_db"
-  providers = {
-    snowflake = snowflake.administrator
-  }
 }
 
 module "users_roles" {
   source = "../../modules/horizon_catalog/users_roles"
   default_password = var.default_password
+  providers = {
+    snowflake.useradmin = snowflake.useradmin
+  }
 }
