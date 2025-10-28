80dc1912c959fa53c67db2cdf253cf84ca0fa843
yamamoto10152 <yamamoto10152@ashisuto.co.jp>
2025-10-28 14:49:33 +0900

    remove integration2

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,4 +1,4 @@
-# Prod環境のメイン設定
+# Prod
 terraform {
   required_version = ">= 1.0"
   required_providers {
@@ -14,18 +14,6 @@ terraform {
     dynamodb_table = "yamamoto-terraform-state-lock-table-prod"
     encrypt        = true
   }
-}
-
-provider "snowflake" {
-  organization_name = var.snowflake_organization_name
-  account_name      = var.snowflake_account_name
-  user              = var.snowflake_user
-  token             = var.snowflake_password
-  role              = var.snowflake_role
-  authenticator     = "PROGRAMMATIC_ACCESS_TOKEN"
-  
-  # プレビュー機能を有効化
-  preview_features_enabled = ["snowflake_table_resource"]
 }
 
 # データベース
