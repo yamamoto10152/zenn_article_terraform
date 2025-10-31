93f0c742974072cf05c9ca9f0c873507576c1c36
yamamoto10152 <yamamoto10152@ashisuto.co.jp>
2025-10-31 12:48:11 +0900

    change integration

--- modules/integrations/main.tf
+++ modules/integrations/main.tf
@@ -1,7 +1,7 @@
 # Storage Integration
 resource "snowflake_storage_integration" "s3_integration" {
   name                      = "S3_INTEGRATION"
-  comment                   = "AWS S3 integration"
+  comment                   = "For yamamoto10152"
   type                      = "EXTERNAL_STAGE"
   enabled                   = true
   storage_allowed_locations = ["s3://${var.s3_bucket_name}/snowpipe/"]
